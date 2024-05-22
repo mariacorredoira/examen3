c265be3a68418c54519eb5409ea88e2d1774aec7
persona <persona@10.0.2.15>
2024-05-22 20:47:03 +0200

    Commit final

--- src/examen3refactorizado/CCuenta.java
+++ src/examen3refactorizado/CCuenta.java
@@ -1,28 +1,44 @@
 package examen3refactorizado;
+/**
+ * @author María Corredoira Martínez 22/05/2024
+ */
+
+/**
+ * Este es el nombre de la clase
+ * @see  CCuenta
+ */
 public class CCuenta {
     // Propiedades de la Clase Cuenta
-    public double dSaldo;
-    public String nombre;
+    private double dSaldo;
+    private String nombre;
 
-    public static void main(String[] args) {
+    public static void main(String[] args, float cantidad) {
 
-        CCuenta cuenta1 = new CCuenta();
-
-        System.out.println("Saldo Incial: " + cuenta1.dSaldo + " euros");
-
-        cuenta1.ingresar(-100);
-        System.out.println("Saldo Incial: " + cuenta1.dSaldo + " euros");
-        cuenta1.ingresar(100);
-        System.out.println("Saldo tras ingreso: " + cuenta1.dSaldo + " euros");
-        cuenta1.ingresar(2000);
-        System.out.println("Saldo tras ingreso: " + cuenta1.dSaldo + " euros");
-
-        cuenta1.ingresar(3000);
-        System.out.println("Saldo tras ingreso: " + cuenta1.dSaldo + " euros");
-        cuenta1.retirar(50);
-        System.out.println("Saldo tras retirada: " + cuenta1.dSaldo + " euros");
+        operativa_cuenta();
     }
 
+	private static void operativa_cuenta() {
+		CCuenta cuenta1 = new CCuenta();
+
+        System.out.println("Saldo Incial: " + cuenta1.getdSaldo() + " euros");
+
+        cuenta1.ingresar(-100);
+        System.out.println("Saldo Incial: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.ingresar(100);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.ingresar(2000);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+
+        cuenta1.ingresar(3000);
+        System.out.println("Saldo tras ingreso: " + cuenta1.getdSaldo() + " euros");
+        cuenta1.retirar(50);
+        System.out.println("Saldo tras retirada: " + cuenta1.getdSaldo() + " euros");
+	}
+/**
+ * 
+ * @param cantidad esta es la cantidad que se permite ingresar en el método ingresar
+ * @return iCodErr
+ */
     /* Modificado para el primer commit */
     public int ingresar(double cantidad) {
         int iCodErr;
@@ -33,11 +49,11 @@ public class CCuenta {
         } else if (cantidad == 3002) {
             System.out.println(
                     "Le recordamos que estamos obligados a notificar a Hacienda dicho ingreso (superior o igual a 3002€)");
-            dSaldo = dSaldo + cantidad;
+            setdSaldo(getdSaldo() + cantidad);
             iCodErr = 0;
         } else {
-            dSaldo = dSaldo + cantidad;
-            iCodErr = 0;
+            setdSaldo(getdSaldo() + cantidad);
+            iCodErr = 2;
         }
 
         return iCodErr;
@@ -46,14 +62,47 @@ public class CCuenta {
     /*
      * Metodo para retirar cantidades en la cuenta. Modifica el saldo.
      */
+    /**
+     * 
+     * @param cantidad esta es la cantidad que se va a retirar
+     */
     public void retirar(double cantidad) {
         if (cantidad <= 0) {
             System.out.println("No se puede retirar una cantidad negativa");
-        } else if (dSaldo < cantidad) {
+        } else if (getdSaldo() < cantidad) {
             System.out.println("No  hay suficiente saldo");
         } else {
-            dSaldo = dSaldo - cantidad;
-            System.out.println("Le quedan " + dSaldo + " en cuenta");
+            setdSaldo(getdSaldo() - cantidad);
+            System.out.println("Le quedan " + getdSaldo() + " en cuenta");
         }
     }
+
+    /**
+     * valor devuelto del método saldo
+     * @return Saldo
+     */
+	public double getdSaldo() {
+		return dSaldo;
+	}
+/**
+ * metodo dSaldo
+ * @param dSaldo 
+ */
+	public void setdSaldo(double dSaldo) {
+		this.dSaldo = dSaldo;
+	}
+/**
+ * valor devuelto para el nombre
+ * @return nombre
+ */
+	public String getNombre() {
+		return nombre;
+	}
+/**
+ * metodo para nombre 
+ * @param nombre
+ */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
